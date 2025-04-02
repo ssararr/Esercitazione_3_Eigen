3358401857d48d60685a96dc4e725fadd0db9544
ssararr <s311121@studenti.polito.it>
2025-04-02 12:39:14 +0200

    Aggiunto esercizio 2

---
+++ Exercise2/Debug/CMakeFiles/LinearSystem.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinearSystem.dir/main.cpp.o"
+  "CMakeFiles/LinearSystem.dir/main.cpp.o.d"
+  "LinearSystem"
+  "LinearSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinearSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Exercise2/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include "Eigen/Eigen"
+
+using namespace std;
+using namespace Eigen;
+
+//fun per risolvere
+void solve(Matrix2d A, Vector2d b)
+{
+	Vector2d x_es;
+	x_es<<-1.0, -1.0;
+	Vector2d x_PALU = A.fullPivLu().solve(b);
+	Vector2d x_QR = A.householderQr().solve(b);
+	double err_PALU = (x_PALU-x_es).norm()/x_es.norm();
+	double err_QR = (x_QR-x_es).norm()/x_es.norm();
+	
+	cout << "La soluzione del sistema tramite decomposizione PALU è " << scientific << setprecision(16) << x_PALU << endl;
+	cout << "L'errore del sistema tramite decomposizione PALU è " << scientific << setprecision(16) << err_PALU << endl;
+	cout << "La soluzione del sistema tramite decomposizione QR è " << scientific << setprecision(16) << x_QR << endl;
+	cout << "L'errore del sistema tramite decomposizione QR è " << scientific << setprecision(16) << err_QR << endl;
+} 
+
+
+
+int main()
+{	
+	//sistema 1
+	cout << "Sistema 1" << endl;	
+	Matrix2d A1;
+	A1 <<5.547001962252291e-01, -3.770900990025203e-02, 
+		8.320502943378437e-01,	-9.992887623566787e-01;
+	Vector2d b1;
+	b1<<-5.169911863249772e-01, 1.672384680188350e-01;
+	
+	solve(A1, b1);
+	
+	
+	
+	//sistema 2
+	cout << "Sistema 2" << endl;	
+	Matrix2d A2;
+	A2 <<5.547001962252291e-01, -5.540607316466765e-01,
+	8.320502943378437e-01, -8.324762492991313e-01;
+	Vector2d b2;
+	b2<<-6.394645785530173e-04, 4.259549612877223e-04;
+	
+	solve(A2, b2);
+	
+	
+	//sistema 2
+	cout << "Sistema 3" << endl;	
+	Matrix2d A3;
+	A3 <<5.547001962252291e-01, -5.547001955851905e-01,
+	8.320502943378437e-01, -8.320502947645361e-01;
+	Vector2d b3;
+	b3<<-6.400391328043042e-10,	4.266924591433963e-10;
+	
+	solve(A3, b3);
+	
+    return 0;
+}
